799aa71de62d9278b378c5d1b63680f9f1c9209d
wolframdeus <wolfram.deus@gmail.com>
2020-03-26 14:38:42 +0500

    Updated ignore

---
+++ test/compiled/answerQuestionMutation.ts
@@ -0,0 +1,51 @@
+import gql from 'graphql-tag';
+import { DocumentNode } from 'graphql';
+
+export namespace AnswerQuestionMutation {
+  export interface Arguments {
+    questionIndex: number;
+    optionIndex: number;
+  }
+  export interface AnswerQuestion {
+    currentQuestion: AnswerQuestion.CurrentQuestion;
+    currentSpeed: AnswerQuestion.CurrentSpeed;
+    currentServerTime: AnswerQuestion.CurrentServerTime;
+    startedAt: AnswerQuestion.StartedAt;
+  }
+  export namespace AnswerQuestion {
+    export interface CurrentQuestion {
+      title: CurrentQuestion.Title;
+      selectedOptions: CurrentQuestion.SelectedOptions;
+      sentAt: CurrentQuestion.SentAt;
+      options: CurrentQuestion.Options;
+      index: CurrentQuestion.Index;
+    }
+    export namespace CurrentQuestion {
+      export type Title = string;
+      export type SelectedOptions = number[];
+      export type SentAt = number;
+      export type Options = string[];
+      export type Index = number;
+    }
+    export type CurrentSpeed = number;
+    export type CurrentServerTime = number;
+    export type StartedAt = number;
+  }
+}
+export interface AnswerQuestionMutation {
+  answerQuestion: AnswerQuestionMutation.AnswerQuestion;
+}
+export const answerQuestionMutation: DocumentNode = gql(`mutation answerQuestion($questionIndex: Int!, $optionIndex: Int!) {
+  answerQuestion(questionIndex: $questionIndex, optionIndex: $optionIndex) {
+    currentQuestion {
+      title
+      selectedOptions
+      sentAt
+      options
+      index
+    }
+    currentSpeed
+    currentServerTime
+    startedAt
+  }
+}`);
---
+++ test/compiled/updateCostumeColorMutation.ts
@@ -0,0 +1,17 @@
+import gql from 'graphql-tag';
+import { DocumentNode } from 'graphql';
+
+import { COLOR } from './schema';
+
+export namespace UpdateCostumeColorMutation {
+  export interface Arguments {
+    color: COLOR;
+  }
+  export type UpdateCostumeColor = boolean;
+}
+export interface UpdateCostumeColorMutation {
+  updateCostumeColor: UpdateCostumeColorMutation.UpdateCostumeColor;
+}
+export const updateCostumeColorMutation: DocumentNode = gql(`mutation updateCostumeColor($color: COLOR!) {
+  updateCostumeColor(color: $color)
+}`);
