733c3534899dd5ee30b26a70be5160cb29f4fb36
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-11-13 18:36:08 +0500

    Updates

---
+++ assets/(GenTextView)-qXAjTJLo.js
@@ -0,0 +1,1 @@
+import{F as e,S as t,Y as n,b as r,p as i,x as a}from"./vue-router-Bf7xIcuF.js";import"./pinia-DfkjHjbi.js";import{i as o,t as s}from"./ViewBase-CLjkRdDm.js";var c=t({__name:`(GenTextView)`,setup(t){return(t,c)=>(e(),i(s,null,{default:n(()=>[a(o,null,{default:n(()=>[...c[0]||=[r(` Gen text view `,-1)]]),_:1})]),_:1}))}});export{c as default};
